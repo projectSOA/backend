e6487ba5b82aee36c696361c730f06c85386bca5
Ismail-Ah <ahakayismail4@gmail.com>
2025-10-24 22:27:59 +0100

    create dtos for the bus entity

--- TripManagementService/src/main/java/dto/bus/BusDetailsDTO.java
+++ TripManagementService/src/main/java/dto/bus/BusDetailsDTO.java
@@ -1,4 +1,21 @@
 package dto.bus;
 
+import dto.route.RouteResponseDTO;
+import enums.BusStatus;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Data
 public class BusDetailsDTO {
+    private UUID id;
+    private String registrationNumber;
+    private Integer capacity;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private BusStatus busStatus;
+    private LocalDate lastMaintenance;
+    private RouteResponseDTO route;
 }
--- TripManagementService/src/main/java/dto/bus/BusRequestDTO.java
+++ TripManagementService/src/main/java/dto/bus/BusRequestDTO.java
@@ -1,4 +1,24 @@
 package dto.bus;
 
+import enums.BusStatus;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Data
 public class BusRequestDTO {
+    @NotNull(message = "Registration number is required")
+    private String registrationNumber;
+
+    private Integer capacity;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private BusStatus busStatus;
+    private LocalDate lastMaintenance;
+
+    @NotNull(message = "Route ID is required")
+    private UUID routeId;
 }
