a704a86815d789e12da981086db105d3db4a4610
Ismail-Ah <ahakayismail4@gmail.com>
2025-10-24 22:27:38 +0100

    create dtos for the stop entity

--- TripManagementService/src/main/java/dto/stop/StopDetailsDTO.java
+++ TripManagementService/src/main/java/dto/stop/StopDetailsDTO.java
@@ -1,4 +1,16 @@
 package dto.stop;
 
+import dto.routeStop.RouteStopResponseDTO;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
 public class StopDetailsDTO {
+    private UUID id;
+    private String name;
+    private Double latitude;
+    private Double longitude;
+    private String address;
+    private RouteStopResponseDTO routeStop;
 }
--- TripManagementService/src/main/java/dto/stop/StopResponseDTO.java
+++ TripManagementService/src/main/java/dto/stop/StopResponseDTO.java
@@ -1,4 +1,14 @@
 package dto.stop;
 
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
 public class StopResponseDTO {
+    private UUID id;
+    private String name;
+    private Double latitude;
+    private Double longitude;
+    private String address;
 }
