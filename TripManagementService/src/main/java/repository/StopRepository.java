ef107f9b8f9c3f0ae7f785b431f6f41be87ac7ce
Ismail-Ah <ahakayismail4@gmail.com>
2025-10-24 22:58:43 +0100

    create stop repository

--- TripManagementService/src/main/java/repository/StopRepository.java
+++ TripManagementService/src/main/java/repository/StopRepository.java
@@ -1,4 +1,10 @@
 package repository;
 
-public interface StopRepository {
+import entity.Stop;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface StopRepository extends JpaRepository<Stop, UUID> {
+
 }
