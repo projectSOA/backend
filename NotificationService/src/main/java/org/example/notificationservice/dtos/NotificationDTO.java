29f329074c29cb86c1440c040b73135ca3f9920d
Youssef Bouraoui <bouraouiyoussef12@gmail.com>
2025-10-26 23:46:57 +0100

    added notification info in notifcaiton dto

--- NotificationService/src/main/java/org/example/notificationservice/dtos/NotificationDTO.java
+++ NotificationService/src/main/java/org/example/notificationservice/dtos/NotificationDTO.java
@@ -3,11 +3,13 @@ package org.example.notificationservice.dtos;
 import org.example.notificationservice.entities.NotificationType;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 
 public record NotificationDTO(
         String id,
         String title,
+        Map<String, Object> notificationInfo,
         NotificationType type,
         String message,
         LocalDateTime sentAt,
