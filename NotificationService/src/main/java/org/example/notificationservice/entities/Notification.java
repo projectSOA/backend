3fe7aef82c38587faaf025c076557d1c3fe50b54
Youssef Bouraoui <bouraouiyoussef12@gmail.com>
2025-10-28 09:24:09 +0100

    removed the jpa dependency

--- NotificationService/src/main/java/org/example/notificationservice/entities/Notification.java
+++ NotificationService/src/main/java/org/example/notificationservice/entities/Notification.java
@@ -1,10 +1,11 @@
 package org.example.notificationservice.entities;
 
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
+
 
 import java.time.LocalDateTime;
 import java.util.Map;
