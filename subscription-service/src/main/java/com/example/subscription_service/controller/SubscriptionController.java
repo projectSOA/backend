f85ad8907509d307fa9012728784cbc85646164a
HAMZA BENHAMMOU <168167755+H-Benhammou@users.noreply.github.com>
2025-11-16 21:32:58 +0100

    Update API endpoint for SubscriptionController

--- subscription-service/src/main/java/com/example/subscription_service/controller/SubscriptionController.java
+++ subscription-service/src/main/java/com/example/subscription_service/controller/SubscriptionController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/subscriptions")
+@RequestMapping("/api/v1/subscriptions")
 public class SubscriptionController {
 
     private final SubscriptionService subscriptionService;
