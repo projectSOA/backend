1107899ababa4bc38ceedb386bb2ac8dc0182cff
Ismail-Ah <ahakayismail4@gmail.com>
2025-11-16 15:20:08 +0100

    implement the stop controller

--- TripManagementService/src/main/java/org/example/tripmanagementservice/controller/RouteStopController.java
+++ TripManagementService/src/main/java/org/example/tripmanagementservice/controller/RouteStopController.java
@@ -1,19 +1,14 @@
 package org.example.tripmanagementservice.controller;
 
+import org.example.tripmanagementservice.dto.routeStop.RouteStopRequestDTO;
 import org.example.tripmanagementservice.dto.routeStop.RouteStopResponseDTO;
-import org.example.tripmanagementservice.dto.stop.StopRequestDTO;
-import org.example.tripmanagementservice.dto.stop.StopResponseDTO;
 import org.example.tripmanagementservice.entity.RouteStop;
-import org.example.tripmanagementservice.entity.Stop;
 import org.example.tripmanagementservice.mapper.RouteStopMapper;
-import org.example.tripmanagementservice.mapper.StopMapper;
 import org.example.tripmanagementservice.service.RouteStopService;
-import org.example.tripmanagementservice.service.StopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -28,27 +23,15 @@ public class RouteStopController {
         this.routeStopService = routeStopService;
     }
 
-    @GetMapping
-    public ResponseEntity<List<RouteStopResponseDTO>> getRouteStops(){
-        List<RouteStop> routeStops = routeStopService.getRouteStops();
-        return ResponseEntity.ok(routeStopMapper.toListResponseDTO(routeStops));
-    }
-
     @GetMapping("/{routeStopId}")
-    public ResponseEntity<StopResponseDTO> getStop(@PathVariable UUID stopId){
-        Stop stop = stopService.getStop(stopId);
-        return ResponseEntity.ok(stopMapper.toResponseDTO(stop));
-    }
-
-    @PostMapping
-    public ResponseEntity<StopResponseDTO> addStop(@RequestBody StopRequestDTO stopRequestDTO){
-        Stop stop = stopService.createStop(stopMapper.toEntity(stopRequestDTO));
-        return ResponseEntity.ok(stopMapper.toResponseDTO(stop));
+    public ResponseEntity<RouteStopResponseDTO> getStop(@PathVariable UUID routeStopId){
+        RouteStop routeStop = routeStopService.getRouteStopById(routeStopId);
+        return ResponseEntity.ok(routeStopMapper.toResponseDTO(routeStop));
     }
 
     @PutMapping("/{stopId}")
-    public ResponseEntity<StopResponseDTO> updateStop(@PathVariable UUID stopId, @RequestBody StopRequestDTO stopRequestDTO){
-        Stop stop = stopService.updateStop(stopId,stopRequestDTO);
-        return ResponseEntity.ok(stopMapper.toResponseDTO(stop));
+    public ResponseEntity<RouteStopResponseDTO> updateRouteStopToRoute(@PathVariable UUID stopId, @RequestBody RouteStopRequestDTO routeStopRequestDTO){
+        RouteStop routeStop = routeStopService.updateRouteStop(stopId,routeStopRequestDTO);
+        return ResponseEntity.ok(routeStopMapper.toResponseDTO(routeStop));
     }
 }
--- TripManagementService/src/main/java/org/example/tripmanagementservice/controller/StopController.java
+++ TripManagementService/src/main/java/org/example/tripmanagementservice/controller/StopController.java
@@ -1,4 +1,52 @@
 package org.example.tripmanagementservice.controller;
 
+import jakarta.validation.Valid;
+import org.example.tripmanagementservice.dto.stop.StopRequestDTO;
+import org.example.tripmanagementservice.dto.stop.StopResponseDTO;
+import org.example.tripmanagementservice.entity.Stop;
+import org.example.tripmanagementservice.mapper.StopMapper;
+import org.example.tripmanagementservice.service.StopService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/stops")
 public class StopController {
+
+    private final StopMapper stopMapper;
+    private final StopService stopService;
+
+    @Autowired
+    public StopController(StopMapper stopMapper, StopService stopService){
+        this.stopMapper = stopMapper;
+        this.stopService = stopService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<StopResponseDTO>> getStops(){
+        List<Stop> stops = stopService.getStops();
+        return ResponseEntity.ok(stopMapper.toListResponseDTO(stops));
+    }
+
+    @GetMapping("/{stopId}")
+    public ResponseEntity<StopResponseDTO> getStop(@PathVariable UUID stopId){
+        Stop stop = stopService.getStop(stopId);
+        return ResponseEntity.ok(stopMapper.toResponseDTO(stop));
+    }
+
+    @PostMapping
+    public ResponseEntity<StopResponseDTO> addStop(@Valid @RequestBody StopRequestDTO stopRequestDTO){
+        Stop stop = stopService.createStop(stopMapper.toEntity(stopRequestDTO));
+        return ResponseEntity.ok(stopMapper.toResponseDTO(stop));
+    }
+
+    @PutMapping("/{stopId}")
+    public ResponseEntity<StopResponseDTO> updateStop(@Valid @PathVariable UUID stopId, @RequestBody StopRequestDTO stopRequestDTO){
+        Stop stop = stopService.updateStop(stopId,stopRequestDTO);
+        return ResponseEntity.ok(stopMapper.toResponseDTO(stop));
+    }
 }
