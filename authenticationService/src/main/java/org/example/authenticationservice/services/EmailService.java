67370646d03de78f0c6818527d7d874e1deb3f66
Ismail-Ah <ahakayismail4@gmail.com>
2025-11-18 00:30:46 +0100

    Add service for email

--- authenticationService/src/main/java/org/example/authenticationservice/services/EmailService.java
+++ authenticationService/src/main/java/org/example/authenticationservice/services/EmailService.java
@@ -1,4 +1,5 @@
 package org.example.authenticationservice.services;
 
 public interface EmailService {
+    void sendPasswordEmail(String toEmail,String password);
 }
--- authenticationService/src/main/java/org/example/authenticationservice/services/impl/EmailServiceImpl.java
+++ authenticationService/src/main/java/org/example/authenticationservice/services/impl/EmailServiceImpl.java
@@ -1,4 +1,24 @@
 package org.example.authenticationservice.services.impl;
 
-public class EmailServiceImpl {
+import org.example.authenticationservice.services.EmailService;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public EmailServiceImpl(JavaMailSender mailSender){
+        this.mailSender = mailSender;
+    }
+
+    public void sendPasswordEmail(String toEmail,String password){
+        SimpleMailMessage message =  new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject("Your Account Password");
+        message.setText("Hello,\n\nYour account has been created. Your password is: " + password + "\n\nPlease change it after logging in.");
+        mailSender.send(message);
+    }
 }
