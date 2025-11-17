072f1023ecdc5dcd70cec09dfa983304efc5c306
Ismail-Ah <ahakayismail4@gmail.com>
2025-11-18 00:33:22 +0100

    add endpoint to create driver

--- authenticationService/src/main/java/org/example/authenticationservice/controller/AuthController.java
+++ authenticationService/src/main/java/org/example/authenticationservice/controller/AuthController.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import org.example.authenticationservice.dtos.CreateUserRequestDTO;
 import org.example.authenticationservice.dtos.LoginRequestDTO;
 import org.example.authenticationservice.dtos.UserDTO;
+import org.example.authenticationservice.entities.User;
 import org.example.authenticationservice.exception.AccountNotActivatedException;
 import org.example.authenticationservice.exception.EmailAlreadyExistsException;
+import org.example.authenticationservice.mappers.UserMapper;
 import org.example.authenticationservice.services.AuthService;
+import org.example.authenticationservice.services.EmailService;
 import org.example.authenticationservice.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,6 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.SecureRandom;
 import java.util.Map;
 import java.util.UUID;
 
@@ -24,11 +28,19 @@ public class AuthController {
 
     private final AuthService authService;
     private final UserService userService;
+    private UserMapper userMapper;
+    private EmailService emailService;
+
+    private static final Integer PASSWORD_LENGTH = 6;
+    private final SecureRandom random;
 
     @Autowired
-    public AuthController(UserService userService,AuthService authService){
+    public AuthController(UserService userService, AuthService authService, UserMapper userMapper, EmailService emailService){
         this.userService = userService;
         this.authService = authService;
+        this.userMapper = userMapper;
+        this.random = new SecureRandom();
+        this.emailService = emailService;
     }
     // we should add sign in and sign up , and creating user like driver only a manager can do that and an admin create a manager
 
@@ -69,6 +81,38 @@ public class AuthController {
     public ResponseEntity<UserDTO> updateUser(@RequestBody UserDTO userDTO){
         UserDTO user = userService.updateUser(userDTO);
         return  ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/add-driver")
+    public ResponseEntity<?> signUp(@RequestBody UserDTO userDTO) {
+        User user = userMapper.fromUserDTO_to_User(userDTO);
+        String password = generatePassword();
+        user.setPassword(password);
+        CreateUserRequestDTO createUserRequest = userMapper.fromUser_to_CreateUserRequestDTO(user);
+        try {
+            authService.SignUp(createUserRequest);
+            emailService.sendPasswordEmail(createUserRequest.email(),password);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch(EmailAlreadyExistsException e){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("email already exists");
+        }
+
+    }
+
+    private String generatePassword(){
+        StringBuilder password = new StringBuilder(PASSWORD_LENGTH);
+        for (int i=0;i<6;i++){
+            if(random.nextBoolean()){
+                char letter = (char) (random.nextBoolean()
+                        ? 'A' + random.nextInt(26)
+                        : 'a' + random.nextInt(26));
+                password.append(letter);
+            }
+            else{
+                password.append(random.nextInt(10));
+            }
+        }
+        return password.toString();
     }
 
 
