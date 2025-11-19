14af6209cce1d2cceec7b0f3db6363251cdba212
AymanANNA <ayman.anna111@gmail.com>
2025-11-19 23:55:43 +0100

    add UserDTO and Role

--- TicketManagementService/src/main/java/org/example/ticketmanagementservice/dto/UserDTO.java
+++ TicketManagementService/src/main/java/org/example/ticketmanagementservice/dto/UserDTO.java
@@ -1,4 +1,14 @@
 package org.example.ticketmanagementservice.dto;
 
-public record UserDTO() {
-}
+import java.util.UUID;
+
+public record UserDTO(
+        UUID id,
+        String firstName,
+        String lastName,
+        String email,
+        String phoneNumber,
+        String password,
+        Role role,
+        boolean isAccountActivated
+) {}
