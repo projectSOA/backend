e046da177b34143184d915b7ed744c9e55793e91
AymanANNA <ayman.anna111@gmail.com>
2025-11-21 22:52:48 +0100

    more fixes

--- TripManagementService/src/main/java/org/example/tripmanagementservice/mapper/RouteMapper.java
+++ TripManagementService/src/main/java/org/example/tripmanagementservice/mapper/RouteMapper.java
@@ -35,25 +35,52 @@ public interface RouteMapper {
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateEntityFromDto(RouteRequestDTO dto, @MappingTarget Route entity);
 
-    default String getStartStationName(Route route){
-        Stop start = route.getRouteStops().stream().filter((stop)->stop.getStopOrder()==0).map(RouteStop::getStop).findFirst().orElse(null);
-        return start!=null?start.getName():"";
+    default String getStartStationName(Route route) {
+        if (route.getRouteStops() == null || route.getRouteStops().isEmpty()) {
+            return "";
+        }
+        return route.getRouteStops().stream()
+                .sorted((a, b) -> Integer.compare(a.getStopOrder(), b.getStopOrder()))
+                .map(RouteStop::getStop)
+                .map(Stop::getName)
+                .findFirst()
+                .orElse("");
     }
 
-    default String getEndStationName(Route route){
-        int numberOfStations = route.getRouteStops().size();
-        Stop end = route.getRouteStops().stream().filter((stop)->stop.getStopOrder()==numberOfStations-1).map(RouteStop::getStop).findFirst().orElse(null);
-        return end!=null?end.getName():"";
+    default String getEndStationName(Route route) {
+        if (route.getRouteStops() == null || route.getRouteStops().isEmpty()) {
+            return "";
+        }
+        return route.getRouteStops().stream()
+                .sorted((a, b) -> Integer.compare(a.getStopOrder(), b.getStopOrder()))
+                .map(RouteStop::getStop)
+                .map(Stop::getName)
+                .reduce((first, second) -> second) // gets last element
+                .orElse("");
     }
 
-    default StationDTO getStartStation(Route route, @Context StopMapper stopMapper){
-        Stop start = route.getRouteStops().stream().filter((stop)->stop.getStopOrder()==0).map(RouteStop::getStop).findFirst().orElse(null);
-        return stopMapper.toStationDTO(start);
+
+    default StationDTO getStartStation(Route route, @Context StopMapper stopMapper) {
+        if (route.getRouteStops() == null || route.getRouteStops().isEmpty()) {
+            return null;
+        }
+        return route.getRouteStops().stream()
+                .sorted((a, b) -> Integer.compare(a.getStopOrder(), b.getStopOrder()))
+                .map(RouteStop::getStop)
+                .findFirst()
+                .map(stopMapper::toStationDTO)
+                .orElse(null);
     }
 
-    default StationDTO getEndStation(Route route, @Context StopMapper stopMapper){
-        int numberOfStations = route.getRouteStops().size();
-        Stop end = route.getRouteStops().stream().filter((stop)->stop.getStopOrder()==numberOfStations-1).map(RouteStop::getStop).findFirst().orElse(null);
-        return stopMapper.toStationDTO(end);
+    default StationDTO getEndStation(Route route, @Context StopMapper stopMapper) {
+        if (route.getRouteStops() == null || route.getRouteStops().isEmpty()) {
+            return null;
+        }
+        return route.getRouteStops().stream()
+                .sorted((a, b) -> Integer.compare(a.getStopOrder(), b.getStopOrder()))
+                .map(RouteStop::getStop)
+                .reduce((first, second) -> second)
+                .map(stopMapper::toStationDTO)
+                .orElse(null);
     }
 }
